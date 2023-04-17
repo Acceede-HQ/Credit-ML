260517b0e5f9fd9f840b5db043ce0471217d0ac1
Samuel Emmanuel <58962390+Sam-Emma@users.noreply.github.com>
2023-04-17 17:28:07 +0100

    Add files via upload

--- Statement.py
+++ Statement.py
@@ -3,14 +3,16 @@ from pydantic import BaseModel
 
 class FinancialStatement(BaseModel):
     account_id: str
-    user_id: str
-    account: dict
-    choose_wards: list
-    approved_wards: list
-    total_wards_fee: float
-    total_approved_wards_fee: float
-    amount_to_payback: float
-    interest_on_amount: float
-    length_of_payback: int
-    amount_to_pay_monthly: float
-    fully_paid: bool
+    # user: str
+    loanReference: str
+    # account: dict
+    # choose_wards: list
+    # approved_wards: list
+    # total_wards_fee: float
+    # total_approved_wards_fee: float
+    # amount_to_payback: float
+    # interest_on_amount: float
+    # length_of_payback: int
+    # amount_to_pay_monthly: float
+    # fully_paid: bool
+    
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import json
 api_keys = [
    "MLS-PASS","asd"
 ]  # This is encrypted in the database
-BASE_URL = "https://dev-djnt.onrender.com"
+BASE_URL = "https://staging-s55s.onrender.com"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
 
 
@@ -24,16 +24,17 @@ def api_key_auth(api_key: str = Depends(oauth2_scheme)):
         )
 
 app = FastAPI()
-#import nest_asyncio
-#nest_asyncio.apply()
+import nest_asyncio
+nest_asyncio.apply()
 
 
 
 @app.post('/',dependencies=[Depends(api_key_auth)])
 def display_analysis(input_data:FinancialStatement):
     account_id=input_data.account_id
-    user_id = input_data.user_id
-    print(user_id)
+    #user_id = input_data.user_id
+    loan_reference = input_data.loanReference
+    print(loan_reference)
     print(account_id)
     try:
         df = get_transactions(account_id)
@@ -47,35 +48,38 @@ def display_analysis(input_data:FinancialStatement):
         
         final_resp = {
             
-          "user": user_id,
-          "account_id": account_id,
-          "account": input_data.account,
-          "choose_wards": input_data.choose_wards,
-          "approved_wards": input_data.approved_wards,
-          "total_wards_fee": input_data.total_wards_fee,
-          "total_approved_wards_fee": input_data.total_approved_wards_fee,
+          # "user": user_id,
+          # "account_id": account_id,
+          "account": input_data.account_id,
+          # "choose_wards": input_data.choose_wards,
+          # "approved_wards": input_data.approved_wards,
+          # "total_wards_fee": input_data.total_wards_fee,
+          # "total_approved_wards_fee": input_data.total_approved_wards_fee,
           "mls": ml_resp,
-          "amount_to_payback": input_data.amount_to_payback,
-          "interest_on_amount": input_data.interest_on_amount,
-          "length_of_payback": input_data.length_of_payback,
-          "amount_to_pay_monthly": input_data.amount_to_pay_monthly,
-          "fully_paid": input_data.fully_paid
+          # "amount_to_payback": input_data.amount_to_payback,
+          # "interest_on_amount": input_data.interest_on_amount,
+          # "length_of_payback": input_data.length_of_payback,
+          # "amount_to_pay_monthly": input_data.amount_to_pay_monthly,
+          # "fully_paid": input_data.fully_paid
     
             }
         headers = {'content-type': 'application/json',
                    "mls-access-token": "MLS_ACCESS_TOKEN"
                    }
-        url = BASE_URL + '/loans/'+ user_id
+        url = BASE_URL + '/loans/mls/'+ loan_reference
         print(final_resp)
         res = json.dumps(final_resp, default=Analysis.type_converter,
                          allow_nan = True)
         res = json.loads(res)
+        #print("hey")
         response = requests.put(url, 
                                 data = json.dumps(final_resp, default=Analysis.type_converter,
                                                  allow_nan = True),
                                 headers = headers
                                 )
         print("response is ", response.status_code)
+        #print("Pass")
+        #print("the response content is: ", response.text)
     
         return res
     except:
