19a790f96c752844a8ad6c946389bb2949bc61c9
Gideon Oke <okegideon4047@gmail.com>
2024-03-21 19:25:58 +0100

    corrected_2

--- MLModel.py
+++ MLModel.py
@@ -14,8 +14,8 @@ def get_transaction_date_range():
     return start_date, end_date
 
 def get_transactions(account_id):
-    (start_date, end_date) = get_transaction_date_range()
-    url = f"https://api.withmono.com/accounts/{account_id}/transactions?paginate=false&start_date&end_date"
+    start_date, end_date = get_transaction_date_range()
+    url = f"https://api.withmono.com/accounts/{account_id}/transactions?paginate=false&{start_date}&{end_date}"
     headers = {"accept": "application/json",
           "mono-sec-key": "live_sk_CA7jLNrlfqEDOy30yLZi"}
     response = requests.get(url, headers=headers)
