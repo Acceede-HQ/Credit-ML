5d049ac555ec4106d7fcb1127b736d38792b3575
Matthew Oke <matthewoke.ai@gmail.com>
2024-03-22 09:10:26 +0100

    Setup functions for easy dev

--- analysis/ml/MLModel.py
+++ analysis/ml/MLModel.py
@@ -4,14 +4,13 @@ import os, requests
 import pickle
 
 def get_vectorizer_model():
-    filepath = os.path.join('models','vectorizer.sav')
+    filepath = os.path.join(os.path.dirname(__file__)+'/models','vectorizer.sav')
     pickle_in = open(filepath,"rb")
     vectorizer_model =pickle.load(pickle_in)
-    print(vectorizer_model)
     return vectorizer_model
 
 def get_ml_model():
-    modelpath = os.path.join('models','k_means_model.sav')
+    modelpath = os.path.join(os.path.dirname(__file__)+'/models','k_means_model.sav')
     pickle_in = open(modelpath,"rb")
     k_means_model =pickle.load(pickle_in)
     return k_means_model
@@ -26,7 +25,7 @@ def remove_one_two(x):
   return x
 
 def preprocess(data):
-  data = pd.DataFrame(data)
+  if not isinstance(data, pd.DataFrame): data = pd.DataFrame(data)
   data = data.replace(np.nan, 0)
   data['balance'] = data['balance']/100
   data['amount'] = data['amount']/100
