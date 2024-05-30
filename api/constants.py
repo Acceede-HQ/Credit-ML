6bd092f8d913b3f385c5c1b9e2f6145d7a62dcd2
Platayo <89252773+Platayo@users.noreply.github.com>
2024-05-30 08:37:28 +0100

    updated mono api - v2 (#19)
    * updated mono api - v2
    
    * Updated mono api - v2
    
    * Updated MONO URL- v2

--- api/constants.py
+++ api/constants.py
@@ -4,6 +4,6 @@ MONO_SECRET_KEY = "MONO_SECRET_KEY"
 AUTH_TOKENS = "AUTH_TOKENS"
 
 # Urls
-MONO_BASE_URL = "https://api.withmono.com/accounts"
+MONO_BASE_URL = "https://api.withmono.com/v2/accounts"
 BASE_URL = "https://staging-s55s.onrender.com"
 BASE_URL2 = "https://acceedeapi.herokuapp.com"
